a86b3bbd1b08062cbb20affb4318281c8efdad0b
Anthony Kim <anthonymoosungkim@proton.me>
2024-09-16 20:52:41 -0400

    made changes

--- exercises/e2.js
+++ exercises/e2.js
@@ -16,11 +16,6 @@ function rectArea(sideA, sideB) {
 	return sideA * sideB;
 }
 
-var rectOne = rectArea(3, 2);
-
-console.log(rectOne);
-
-
 // Use the function you created above to calculate the areas of 3 rooms
 // Assign the results to room1, room2, and room3 variables
 // Data for calculation: room1: 4 by 6, room2: 13 by 3, room3: 4 by 8
@@ -30,10 +25,6 @@ console.log(rectOne);
 var room1 = rectArea(4, 6);
 var room2 = rectArea(13, 3);
 var room3 = rectArea(4, 8);
-
-console.log(room1);
-console.log(room2);
-console.log(room3);
 
 /**
  * =============================================================
@@ -51,18 +42,11 @@ function totalRoomsArea(area1, area2, area3) {
 	return area1 + area2 + area3;
 }
 
-var sum = totalRoomsArea(24, 35, 24);
-
-console.log(sum);
-
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
-const result = function totalRoomsArea(room1, room2, room3) {
-    console.log(result);
+const result = function (area1, area2, area3) {
+    	console.log(totalRoomsArea(room1, room2, room3);
 }
-
-console.log(result);
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
